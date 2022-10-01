9f0d96143371a155930376aca9d807fe6fedf91a
Ferema10 <mendoza1im15@gmail.com>
2022-10-01 11:20:31 -0500

    rama develop splash screen v2

--- app/src/main/java/com/upiita/mechaponicsapp/MainActivity.java
+++ app/src/main/java/com/upiita/mechaponicsapp/MainActivity.java
@@ -40,6 +40,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 3000);
+        }, 2100);
     }
 }
