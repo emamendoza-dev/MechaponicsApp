93c483fc4a5a88251a6960b44186efd25420d289
Ferema10 <mendoza1im15@gmail.com>
2022-10-01 10:39:57 -0500

    rama develop splash screen v1

--- app/src/main/java/com/upiita/mechaponicsapp/MainActivity.java
+++ app/src/main/java/com/upiita/mechaponicsapp/MainActivity.java
@@ -2,14 +2,44 @@ package com.upiita.mechaponicsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Configuración Splash Screen
+        // Toda la pantalla sin barra de estado de Android
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        // Rama develop
+        // Configuración Splah Screen
+        // Creacion de animaciones
+        Animation animacion1 = AnimationUtils.loadAnimation(this,R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this,R.anim.desplazamiento_abajo);
+        // Extracción de variables del xml
+        TextView tvSCBy = findViewById(R.id.tvSCBy);
+        TextView tvSCName = findViewById(R.id.tvSCName);
+        ImageView ivSCLogo = findViewById(R.id.ivSCLogo);
+        // Agregar animaciones a las variables
+        tvSCBy.setAnimation(animacion2);
+        tvSCName.setAnimation(animacion2);
+        ivSCLogo.setAnimation(animacion1);
+        // Finalizar Splash Screen tras un instante de tiempo
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
