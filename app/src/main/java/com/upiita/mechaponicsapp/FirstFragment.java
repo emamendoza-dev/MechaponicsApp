5f32ee39ddc03daa84dd98f44d3cfffc1b80764e
Ferema10 <mendoza1im15@gmail.com>
2022-10-01 23:56:27 -0500

    rama feature v0.5.5 recycler view con card view Firebase

--- app/src/main/java/com/upiita/mechaponicsapp/FirstFragment.java
+++ app/src/main/java/com/upiita/mechaponicsapp/FirstFragment.java
@@ -1,19 +1,115 @@
 package com.upiita.mechaponicsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirstFragment extends Fragment {
+
+    ListAdapter adapterH;
+    RecyclerView recyclerViewH;
+    ArrayList<ListElement> elements;
+
+    String pathProyecto = "MechaponicsSystem/";
+    String valor1, valor2, valor3, valor4, valor5, valor6, valor7, valor8;
+    int nodos = 1;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        recyclerViewH = view.findViewById(R.id.recyclerView);
+        elements = new ArrayList<>();
+
+        for (int i=0; i<nodos; i++){
+            int valnodo = i+1;
+            String numnodo = Integer.toString(valnodo);
+            String pathNodo = "Nodo" + numnodo;
+            DatabaseReference referenceNodo = database.getReference(pathProyecto+pathNodo);
+
+            referenceNodo.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    valor1 = snapshot.child("Variable1").getValue().toString();
+                    valor2 = snapshot.child("Variable2").getValue().toString();
+                    valor3 = snapshot.child("Variable3").getValue().toString();
+                    valor4 = snapshot.child("Variable4").getValue().toString();
+                    valor5 = snapshot.child("Variable5").getValue().toString();
+                    valor6 = snapshot.child("Variable6").getValue().toString();
+                    valor7 = snapshot.child("Variable7").getValue().toString();
+                    valor8 = snapshot.child("Variable8").getValue().toString();
+
+                    if(elements.size() == 2){
+                        elements.remove(0);
+                        elements.add(0, new ListElement( pathNodo, "Variable 1: "+valor1,
+                                "Variable 2: "+valor2,"Variable 3: "+valor3,
+                                "Variable 4: "+valor4,"Variable 5: "+valor5,
+                                "Variable 6: "+valor6,"Variable 7: "+valor7,
+                                "Variable 8: "+valor8,"Estado OK"));
+                    }else{
+                        elements.add(0, new ListElement( pathNodo, "Variable 1: "+valor1,
+                                "Variable 2: "+valor2,"Variable 3: "+valor3,
+                                "Variable 4: "+valor4,"Variable 5: "+valor5,
+                                "Variable 6: "+valor6,"Variable 7: "+valor7,
+                                "Variable 8: "+valor8,"Estado OK"));
+                    }
+                    init();
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+        //return inflater.inflate(R.layout.fragment_first, container, false);
+        return view;
+    }
+
+    public void init(){
+        recyclerViewH.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapterH = new ListAdapter(getContext(),elements, new ListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(ListElement item) {
+                moveToDescription(item);
+            }
+        });
+        recyclerViewH.setAdapter(adapterH);
+
+        adapterH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cambio = new Intent( getActivity() ,DatosActivity.class);
+                startActivity(cambio);
+                String nombre = elements.get(recyclerViewH.getChildAdapterPosition(view)).getNodo();
+                Toast.makeText(getContext(),"Selecciono: "+nombre, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void moveToDescription(ListElement item){
+        Intent intent = new Intent(getContext(), DatosActivity.class);
+        intent.putExtra("ListElement", item);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/upiita/mechaponicsapp/ListAdapter.java
@@ -0,0 +1,119 @@
+package com.upiita.mechaponicsapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> implements View.OnClickListener{
+    ArrayList<ListElement> mData;
+    LayoutInflater mInflater;
+
+    private View.OnClickListener listener;
+
+    final ListAdapter.OnItemClickListener listener2;
+
+    public interface OnItemClickListener{
+        void onItemClick(ListElement item);
+    }
+
+    public ListAdapter(Context context, ArrayList<ListElement> itemList, ListAdapter.OnItemClickListener listener2){
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = itemList;
+        this.listener2 = listener2;
+    }
+    @Override
+    public int getItemCount(){ return mData.size();}
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = mInflater.inflate(R.layout.list_element, parent, false);
+        view.setOnClickListener(this);
+        return new ViewHolder(view);
+    }
+
+    public void setOnClickListener(View.OnClickListener listener){
+        this.listener = listener;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position){
+        String nodo = mData.get(position).getNodo();
+        String var1 = mData.get(position).getVariable1();
+        String var2 = mData.get(position).getVariable2();
+        String var3 = mData.get(position).getVariable3();
+        String var4 = mData.get(position).getVariable4();
+        String var5 = mData.get(position).getVariable5();
+        String var6 = mData.get(position).getVariable6();
+        String var7 = mData.get(position).getVariable7();
+        String var8 = mData.get(position).getVariable8();
+        String estado = mData.get(position).getEstado();
+
+        holder.nodo.setText(nodo);
+        holder.variable1.setText(var1);
+        holder.variable2.setText(var1);
+        holder.variable3.setText(var1);
+        holder.variable4.setText(var1);
+        holder.variable5.setText(var1);
+        holder.variable6.setText(var1);
+        holder.variable7.setText(var1);
+        holder.variable8.setText(var1);
+        holder.estado.setText(estado);
+        holder.bindData(mData.get(position));
+
+    }
+
+    public void setItems(ArrayList<ListElement> items){mData = items;}
+
+    public void onClick(View view){
+        if(listener!=null){
+            listener.onClick(view);
+        }
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView nodo, variable1, variable2, variable3,
+                variable4, variable5, variable6, variable7, variable8, estado;
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            nodo = itemView.findViewById(R.id.tvNodo);
+            variable1 = itemView.findViewById(R.id.tvVariable1);
+            variable2 = itemView.findViewById(R.id.tvVariable2);
+            variable3 = itemView.findViewById(R.id.tvVariable3);
+            variable4 = itemView.findViewById(R.id.tvVariable4);
+            variable5 = itemView.findViewById(R.id.tvVariable5);
+            variable6 = itemView.findViewById(R.id.tvVariable6);
+            variable7 = itemView.findViewById(R.id.tvVariable7);
+            variable8 = itemView.findViewById(R.id.tvVariable8);
+            estado = itemView.findViewById(R.id.tvEstadoSis);
+        }
+
+        void bindData(final ListElement item){
+            nodo.setText(item.getNodo());
+            variable1.setText(item.getVariable1());
+            variable2.setText(item.getVariable2());
+            variable3.setText(item.getVariable3());
+            variable4.setText(item.getVariable4());
+            variable5.setText(item.getVariable5());
+            variable6.setText(item.getVariable6());
+            variable7.setText(item.getVariable7());
+            variable8.setText(item.getVariable8());
+            estado.setText(item.getEstado());
+            itemView.setOnClickListener((new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener2.onItemClick(item);
+                }
+            }));
+        }
+    }
+}
